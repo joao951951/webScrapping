2aaf3511216d24f31d36680edb5ef9ea8c7a4eaf
Joao Paulo Carnellossi dos Santos <joao951951>
2023-03-09 17:41:54 -0300

    created method find data

--- webScrapping.py
+++ webScrapping.py
@@ -6,3 +6,14 @@ from selenium import webdriver
 import time
 from datetime import date
 import re
+
+regionData = {"PR" : "estado-pr"}
+
+def findData(pages = 5, region = "PR"):
+    pageSufix = "?o="
+    regionData = {"PR" : "estado-pr"}
+    for i in range(1, pages+1):
+        urlOLX = "https://www.olx.com.br/autos-e-pecas/motos/"+regionData[region]+pageSufix+str(i)
+        print(urlOLX)
+
+findData()
